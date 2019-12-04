67cccf1fd5c25d75fc0edb6e130496049c810a14
asandin218 <54902307+asandin218@users.noreply.github.com>
2019-12-04 00:29:40 -0500

    Add files via upload
    Updated the total litter javascript function to display circles in the map. The radius of the circle will increase depending on the number of bags/tires. Added popup to the circles and used different colors to differentiate between bags and tires.

--- PGTrash.js
+++ PGTrash.js
@@ -93,8 +93,11 @@ function loadDataByOrganization(organization) {
       });
 }
 
-function loadDataByTotalBagsLitter(bagsType) {
-   console.log("Filtering for Total Bags Litter: " + bagsType);
+function loadDataByTotalLitter(totalLitterType) {
+   console.log("Filtering for Total Bags Litter: " + totalLitterType);
+   
+   // NOTE: The first thing we do here is clear the markers from the layer.
+   markersLayer.clearLayers();
    
    fetch('/api')
       .then(res => res.json())
@@ -104,26 +107,59 @@ function loadDataByTotalBagsLitter(bagsType) {
               longitude: c.longitude,
               organization: c.organization,
               type_litter: c.type_litter,
-              total_bags_litter: c.total_bags_litter
+              number_bags: c.number_bags,
+              total_tires: c.total_tires,              
           };
       }))
       .then(res => {
-    	  var groups = [];
-    	  for(var index = 0; index < res.length; index++) {
-    		  if (typeof groups[res[index].total_bags_litter] === 'undefined') {
-    			  groups[res[index].total_bags_litter] = new Array();
-    		  }
-
-    		  groups[res[index].total_bags_litter].push({
-    	  			latitude: res[index].latitude,
-    	  			longitude: res[index].longitude,
-    		  });
-    	  }
-    	 if(bagsType === 'all') {
-          return groups;
-       } else {
-         return groups[bagsType];
-       }
+         for(var index = 0; index < res.length; index++) {
+            var circle;
+            var latitude = res[index].latitude;
+            var longitude = res[index].longitude;
+            
+            if(totalLitterType == 'number_bags' || totalLitterType == 'all') {
+               if(res[index].number_bags > 0) {
+                  circle = L.circle([longitude, latitude], {
+                     color: 'red',
+                     fillColor: '#f03',
+                     fillOpacity: 0.5,
+                     radius: res[index].number_bags * 50
+                  });
+                  
+                  // Add a popup to the circle
+                  circle.bindPopup(
+                        "<b>" + res[index].organization + "</b><br>" +
+                        "Total Bags: " + res[index].number_bags
+                  ).openPopup();
+                  
+                  // Add marker to the layer. Not displayed yet.
+                  markersLayer.addLayer(circle);
+               }
+            } 
+            
+            if(totalLitterType == 'total_tires' || totalLitterType == 'all') {
+               if(res[index].total_tires > 0) {
+                  circle = L.circle([longitude, latitude], {
+                     color: 'blue',
+                     fillColor: '#00f',
+                     fillOpacity: 0.5,
+                     radius: res[index].total_tires * 50
+                  });
+                  
+                  // Add a popup to the circle
+                  circle.bindPopup(
+                        "<b>" + res[index].organization + "</b><br>" +
+                        "Total Tires: " + res[index].total_tires
+                  ).openPopup();
+                  
+                  // Add marker to the layer. Not displayed yet.
+                  markersLayer.addLayer(circle);
+               }
+            }
+         }
+         // Display all the markers.
+         markersLayer.addTo(mymap);
+         return res;
       })
       .then(res => {
     	  console.log(res);
