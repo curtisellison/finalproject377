6c2918b1e850106b269a3b62b6a6708b64881466
asandin218 <andresito.sandin@gmail.com>
2019-12-05 02:02:29 -0500

    updated port

--- project_server.js
+++ project_server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const fetch = require("node-fetch");
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 80;
 
 var path = require('path');
 
